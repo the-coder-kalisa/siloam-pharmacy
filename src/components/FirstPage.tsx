750d801e424870442e87b1ef7bd071b91c428ff5
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-20 15:44:34 +0200

    fifth page

--- src/components/FirstPage.tsx
+++ src/components/FirstPage.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@mui/material";
 import React from "react";
+import Rotate from "react-reveal/Rotate";
+import Bounce from "react-reveal/Bounce";
 import Background from "../images/Vector.png";
 const FirstPage: React.FC = () => {
   interface button {
@@ -17,36 +19,40 @@ const FirstPage: React.FC = () => {
       className="bg-center pt-[8rem] pb-[7rem] px-14 flex items-start text-white bg-cover"
     >
       <div className="flex gap-[7rem] flex-col">
-        <div className="flex flex-col gap-10">
-          <div className="text-2xl text-[#E5E5E5]">
-            We work to take care of your lives.
+        <Rotate top left>
+          <div className="flex flex-col gap-10">
+            <div className="text-2xl text-[#E5E5E5]">
+              We work to take care of your lives.
+            </div>
+            <div className="font-bold text-5xl max-w-[40rem]">
+              Be patient with patients who are not patient.
+            </div>
+            <div className="text-2xl max-w-[25rem] text-[#E5E5E5]">
+              Just tell us what you're suffering from and let us cure your.
+            </div>
           </div>
-          <div className="font-bold text-5xl max-w-[40rem]">
-            Be patient with patients who are not patient.
+        </Rotate>
+        <Bounce bottom duration={5000}>
+          <div className="flex gap-4">
+            {buttons.map((button, index) => (
+              <Button
+                key={index}
+                style={{
+                  textDecoration: "uppercase",
+                  color: "white",
+                  border: "2px solid white",
+                  padding: "10px 30px",
+                  fontSize: "1.2rem",
+                  borderRadius: 10,
+                  background: "transparent",
+                }}
+                variant="outlined"
+              >
+                {button.name}
+              </Button>
+            ))}
           </div>
-          <div className="text-2xl max-w-[25rem] text-[#E5E5E5]">
-            Just tell us what you're suffering from and let us cure your.
-          </div>
-        </div>
-        <div className="flex gap-4">
-          {buttons.map((button, index) => (
-            <Button
-              key={index}
-              style={{
-                textDecoration: "uppercase",
-                color: "white",
-                border: "2px solid white",
-                padding: "10px 30px",
-                fontSize: "1.2rem",
-                borderRadius: 10,
-                background: "transparent",
-              }}
-              variant="outlined"
-            >
-              {button.name}
-            </Button>
-          ))}
-        </div>
+        </Bounce>
       </div>
     </div>
   );
