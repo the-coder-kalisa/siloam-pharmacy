603a4395a514fa021b844c0d82a023277272f82b
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-22 22:10:41 +0200

    ad

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-       <Route path="/" element={<App />} />
-       <Route path="/login" element={<Login />} />
-       <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   </React.StrictMode>
