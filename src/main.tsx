1861d9754310c9a10f6cac14fa9ede2172a832bd
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-22 15:58:49 +0200

    adding login and signup

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <App />
+       <Route path="/" element={<App />} />
+       <Route path="/login" element={<Login />} />
+       <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   </React.StrictMode>
