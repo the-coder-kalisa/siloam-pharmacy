9dba61bfd69862f34cbdc7c0f74fb616bfcc8696
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-22 15:55:38 +0200

    fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,41 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@mui/material";
-const Navbar:React.FC =()=> {
+import { Link } from "react-router-dom";
+const Navbar: React.FC = () => {
   const [scrolled, setScrolled] = useState<boolean>(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       window.scrollY >= 540 ? setScrolled(true) : setScrolled(false);
-    })
-  })
-  useEffect(()=>{
+    });
+  });
+  useEffect(() => {
     window.screenY >= 540 ? setScrolled(true) : setScrolled(false);
-  },[])
+  }, []);
   console.log(scrolled);
-    const links: string[] = ["home","about", "services", "demostration"];
+  const links: string[] = ["home", "about", "services", "demostration"];
   return (
     <div
-    style={{background: scrolled ? "#38B295" : "transparent"}}
+      style={{ background: scrolled ? "#38B295" : "transparent" }}
       className={`px-10 py-5 fixed z-50 text-white flex items-center justify-between w-full`}
     >
       <div className="font-bold text-3xl">Siloam</div>
       <div className="flex justify-center text-[#E5E5E5] text-xl font-semibold gap-5 items-center">
-        {links.map( (link, index) => <a key={index} href={`#${link}`}>{link}</a>)}
-        <Button variant="outlined" style={{color: "white", border: "2px solid white"}}>LOGIN</Button>
+        {links.map((link, index) => (
+          <a key={index} href={`#${link}`}>
+            {link}
+          </a>
+        ))}
+          <Button
+            variant="outlined"
+            style={{ color: "white", border: "2px solid white" }}
+          >
+            <Link to="/login">
+            LOGIN
+            </Link>
+          </Button>
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <App />
+      </Routes>
+    </Router>
   </React.StrictMode>
-)
+);
