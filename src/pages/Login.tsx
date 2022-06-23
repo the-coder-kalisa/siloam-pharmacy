09d5d79783c9ecc738935033f8791fd803711b5f
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-23 16:24:29 +0200

    ad

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import LoginSvg from "../images/login";
 
 function Login() {
+  interface Value {
+    email: string,
+    password: string
+  }
+  const inputs : string[] = ["email", "password"];
+  const [values, setValues] = useState<Value>()
   return (
     <div className="h-[100vh] flex items-center justify-center">
       <div className="flex flex-col items-center justify-center">
         <h1 className="font-bold text-3xl">Siloam pharmacy</h1>
         <LoginSvg />
       </div>
-      <form>
-        <h1>Login to Siloam pharmacy</h1>
+      <form className="flex flex-col items-center justify-center bg-[#0A5A64]">
+        <h1 className="font-bold text-white text-3xl">
+          Login to Siloam pharmacy
+        </h1>
+
+        <div>
+          <span className="text-white">Don't have signup yet?</span>
+          <Link to="/signup">signup</Link>
+        </div>
       </form>
     </div>
   );
