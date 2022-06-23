e1f2d1d414d66078ff8e9fd502797f9c1591bbda
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-23 16:16:11 +0200

    adding svgs

--- src/images/login.tsx
+++ src/images/login.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Login() {
+function LoginSvg() {
   return (
     <svg
       data-name="Layer 1"
@@ -162,4 +162,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default LoginSvg;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import LoginSvg from "../images/login";
 
 function Login() {
   return (
     <div>
       <div>
-        
+        <LoginSvg />
       </div>
       <form></form>
     </div>
