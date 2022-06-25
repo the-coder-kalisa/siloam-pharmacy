3db2a0260e43cf66a3f433579a51470f9806b607
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-25 21:55:43 +0200

    1

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { Button, Divider, TextField } from "@mui/material"
 import React, { useState } from "react";
 import { Link } from "react-router-dom"
-import LoginSvg from "../images/login";
+import LoginSvg from "../images/login"
 
 const Login: React.FC = () => {
   interface Value {
