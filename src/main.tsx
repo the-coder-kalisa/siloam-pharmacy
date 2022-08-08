e73bff2ffa4f77e35ee77526a2754a3b6518214a
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-08-08 23:01:36 +0200

    af

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,12 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import { Provider } from "react-redux";
-import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Provider store={store}><App /></Provider>} />
+        <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
